f20ea785d68ca75fbe1951bb92bd42f37133af1a
= <=>
2022-11-14 17:11:31 -0500

    inicio practica formulario nuxt 3

---
+++ components/FormPractice.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mt-10" >
+        <form class="w-1/3 mx-auto p-5 bg-purple-500" method="get">
+            <div class="my-4">
+                <label class="text-white" for="title">Title</label>
+                <input class="block w-full rounded outline-none pl-2" type="text" id="title" placeholder="title">
+            </div>
+            <div>
+                <label class="text-white" for="description">Description</label>
+                <textarea class="block w-full resize-none rounded outline-none pl-2" name="description" id="description"></textarea>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script setup>
+
+</script>
